4dd83643ed0b2055dd7282fd3fe3d1af83084eab
lislis <mail@lislis.de>
2020-03-01 17:43:39 +0100

    update

--- src/space_is_the_place/hilbert.clj
+++ src/space_is_the_place/hilbert.clj
@@ -1,11 +1,14 @@
 (ns space-is-the-place.hilbert
   (:require []))
 
+;; in draw
+;; (hilbert start-x start-y canvas-w 0 0 canvas-h levels)
+
 (defn hilbert [x0 y0 xi xj yi yj level]
   (if (<= level 0)
     (let [x (+ x0 (/ (+ xi yi) 2))
           y (+ y0 (/ (+ xj yj) 2))]
-      {:x x :y y})
+      (q/ellipse x y 2 2))
     (let [a (hilbert x0 y0
                      (/ yi 2) (/ yj 2)
                      (/ xi 2) (/ xj 2) (dec level))
@@ -17,4 +20,5 @@
                      (/ yi 2) (/ yj 2) (dec level))
           d (hilbert (+ x0 (/ xi 2) yi) (+ y0 (/ xj 2) yj)
                      (- (/ yi 2)) (- (/ yj 2))
-                     (- (/ xi 2)) (- (/ xj 2)) (dec level))])))
+                     (- (/ xi 2)) (- (/ xj 2)) (dec level))]
+      (print (:path state)))))
