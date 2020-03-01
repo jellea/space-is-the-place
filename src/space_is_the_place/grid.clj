bf7b53c63ad381cce7645b1de31d8fcd51b6d7e0
Jelle Akkerman <jellea@users.noreply.github.com>
2020-03-01 16:20:07 +0100

    cleanup

--- src/space_is_the_place/grid.clj
+++ src/space_is_the_place/grid.clj
@@ -1,16 +1,6 @@
 (ns space-is-the-place.grid
   (:require [overtone.osc :as osc]))
 
-
-;(defonce grid (mcore/connect "/dev/tty.usbserial-m1000548"))
-;
-;
-;grid
-
-;(:led-set-all grid 12)
-;( grid "/grid/led/set 12")
-;(mled/led-on grid 0 0 15)
-
 (defonce server (osc/osc-server 12003))
 (defonce oscserial (osc/osc-client "localhost" 12002))
 ;(def osculator (osc/osc-client "localhost" 8000))
@@ -24,13 +14,16 @@
 ;(osc/osc-handle server (fn [msg] (println "listener: " msg)) :foo)
 ;
 ;(osc/osc-send-msg grid {:path "grid/led/set" :type-tag "i i i" :args [0 0 15]})
-(osc/osc-send-msg grid (osc/osc-msg "/sys/info" "localhost" 8000))
-(osc/osc-send-msg grid (osc/osc-msg "/sys/port" 12003))
-(osc/osc-send-msg grid (osc/osc-msg "/sys/prefix" "j"))
-(osc/osc-send-msg grid (osc/osc-msg "/j/grid/led/level/set" 0 0 14))
-(osc/osc-send-msg grid (osc/osc-msg "/j/grid/led/level/set" 2 2 14))
+;(osc/osc-send-msg grid (osc/osc-msg "/sys/info" "localhost" 8000))
+;(osc/osc-send-msg grid (osc/osc-msg "/sys/port" 12003))
+;(osc/osc-send-msg grid (osc/osc-msg "/sys/prefix" "j"))
+;(osc/osc-send-msg grid (osc/osc-msg "/j/grid/led/level/set" 0 0 14))
+;(osc/osc-send-msg grid (osc/osc-msg "/j/grid/led/level/set" 2 2 14))
 
-(defn set-led [args])
+(defn set-led [& args]
+  (osc/osc-send-msg grid (apply (partial osc/osc-msg "/j/grid/led/level/set") args)))
+
+(set-led 2 3 8)
 
 (osc/osc-handle server "/j/grid/key" (fn [{[x y z] :args}] (prn "hello" x y z)))
 
