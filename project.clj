6c354fa59ff70bc6b96f5cb4e096e5c01258f46b
Jelle Akkerman <jellea@users.noreply.github.com>
2020-03-01 16:13:42 +0100

    init with some monome stuff

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject space-is-the-place "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [overtone/osc-clj "0.9.0"]
+                 [quil "3.1.0"]])
