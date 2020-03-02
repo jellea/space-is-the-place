f12bc92b8d682d822c6acc53f664ccc4bdfdd843
lislis <mail@lislis.de>
2020-03-02 13:15:03 +0100

    tweak hilbert draw example

--- src/space_is_the_place/hilbert.clj
+++ src/space_is_the_place/hilbert.clj
@@ -2,13 +2,18 @@
   (:require []))
 
 ;; in draw
+;; don't specify a type of shape,
+;; the default is what we want.
+;;
+;; (q/begin-shape)
 ;; (hilbert start-x start-y canvas-w 0 0 canvas-h levels)
+;; (q/end-shape)
 
 (defn hilbert [x0 y0 xi xj yi yj level]
   (if (<= level 0)
     (let [x (+ x0 (/ (+ xi yi) 2))
           y (+ y0 (/ (+ xj yj) 2))]
-      (q/ellipse x y 2 2))
+      (q/vertex x y))
     (let [a (hilbert x0 y0
                      (/ yi 2) (/ yj 2)
                      (/ xi 2) (/ xj 2) (dec level))
@@ -20,5 +25,4 @@
                      (/ yi 2) (/ yj 2) (dec level))
           d (hilbert (+ x0 (/ xi 2) yi) (+ y0 (/ xj 2) yj)
                      (- (/ yi 2)) (- (/ yj 2))
-                     (- (/ xi 2)) (- (/ xj 2)) (dec level))]
-      (print (:path state)))))
+                     (- (/ xi 2)) (- (/ xj 2)) (dec level))])))
